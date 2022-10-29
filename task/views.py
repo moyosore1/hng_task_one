573b0113c8bd81fa35101fc18843a15f0364d9a4
moyosore1 <moyosoreolumideobi@gmail.com>
2022-10-29 03:06:50 -0400

    Update views.py

--- task/views.py
+++ task/views.py
@@ -12,9 +12,7 @@ def endpoint_task(request):
     }
     response =  Response(context)
     response["Access-Control-Allow-Origin"] = "*"
-    response["Access-Control-Allow-Methods"] = "GET, OPTIONS"
-    response["Access-Control-Max-Age"] = "1000"
-    response["Access-Control-Allow-Headers"] = "X-Requested-With, Content-Type"
+    
     return response
 
 
