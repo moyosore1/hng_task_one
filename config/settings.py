e6614190a4b7e9f1d5b263af27fcfc558ce79698
moyosore1 <moyosoreolumideobi@gmail.com>
2022-10-29 02:45:11 -0400

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "moyo-task-one.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
